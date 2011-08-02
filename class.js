8ec90a15c21f13894a2799c6aeca84508d031440
Davide Callegari <admin@brokenseal.it>
2011-08-02 15:20:08 +0200

    Performed some optimizations

--- class.js
+++ class.js
@@ -31,262 +31,290 @@
  */
 
 (function(__global__){
-	var
-		attrs= {
-			__new__: function(classObj){
-				// the instantiator method responsible to give back an instance
-				// of the class 'classObj'
-				
-				return new classObj();
-			}
-			,__init__: function(kwargs){
-				var name;
-				
-				for(name in kwargs) {
-					this[name]= kwargs[name];
-				}
-				
-				return this;
-			}
-			
-			,__name__: 'Class'
-			,__call__: null
-			,__parent__: null
-			
-			,__iter__: function(){ throw new NotImplemented() }
-			
-			,__len__: function(){
-				// The __len__ method returns the length of the object
-				// or the length
-				var
-					nKeys= 0
-					,name
-				;
-				
-				// does it have a length attribute?
-				if(this.length !== undefined) {
-					return this.length;
-				}
-				
-				// find out what's the length of this object based on own properties
-				for(name in this) {
-					if(this.hasOwnProperty(name)) {
-						nKeys+= 1;
-					}
-				}
-				
-				return nKeys;
-			}
-			
-			,__repr__: function(){
-				return "<" + this.__class__.__name__ + ": " + this.__class__.__name__ + " object>";
-			}
-			,__str__: function(){
-				return this.__repr__.apply(this, arguments);
-			}
-			,__unicode__: function(){ throw new NotImplemented() }
-			
-			// js stuff
-			,toString: function(){
-				return this.__repr__.apply(this, arguments);
-			}
-			//,__iterator__: function(){
-			//	if(this.__iter__) {
-			//		return this.__iter__.apply(this, arguments);
-			//	}
-			//	
-			//	return new Iterator(this);
-			//}
-			//,toLocaleString: function(){}
-			//,toSource: function(){}
-			//,valueOf: function(){}
-		}
-		,staticAttrs= {
-			__repr__: function(){
-				return "<class " + this.__name__ + ">";
-			}
-			,toString: function(){
-				return this.__repr__.apply(this, arguments);
-			}
-		}
-		,NotImplemented= new Error('Not implemented.')
-		,klassInitializing= false
-		,fnTest= /xyz/.test(function(){var xyz;}) ? /\b_super\b/ : /.*/
-		,Class= function(kwargs){
-			return;
-		}
-	;
-	
-	// Create a new Class that inherits from the current class.
-	Class.create= function(kwargs) {
-		var
-			_super_class= this
-			,_super= this.prototype
-			,prototype
-			,metaClass= {}
-			,name
-			,__class__
-			,instanceInitializing= false
-		;
-		
-		// metaclass init
-		if(kwargs && kwargs.__metaclass__) {
-			metaClass= kwargs.__metaclass__();
-		}
-		for(name in staticAttrs) {
-			if(!metaClass.hasOwnProperty(name)) {
-				metaClass[name]= staticAttrs[name];
-			}
-		}
-		for(name in kwargs) {
-			if(kwargs[name] && kwargs.hasOwnProperty(name) && kwargs[name].staticMethod) {
-				metaClass[name]= kwargs[name];
-				delete kwargs[name];
-			}
-		}
-		
-		// build the prototype
-		for(name in attrs) {
-			if(kwargs && !kwargs.hasOwnProperty(name)) {
-				kwargs[name]= attrs[name];
-			}
-		}
-		
-		// The dummy class constructor
-		// All construction is actually done in the __new__ method
-		__class__= function(){
-			var
-				newMethod
-				,newInstance
-				,args= Array.prototype.slice.call(arguments)
-			;
-			args.unshift(__class__);
-			
-			if(!(this instanceof __class__)) {
-				newMethod= __class__.prototype.__new__;
-			} else if(!instanceInitializing) {
-				newMethod= this.__new__;
-			}
-			
-			if( !(this instanceof __class__) || !instanceInitializing ) {
-				instanceInitializing= true;
-				newInstance= newMethod.apply(__class__, args);
-				instanceInitializing= false;
-				
-				if ( !klassInitializing && newInstance.__init__ && newInstance instanceof __class__ ) {
-					newInstance.__init__.apply(newInstance, arguments);
-				}
-				
-				if(!newInstance.__call__) {
-					return newInstance;
-				}
-				
-				// allow the new instance of the class to be callable
-				// it's good to make callable objects, but the returned
-				// object not being an instance of __class__
-				callableObject= function(){
-					return newInstance.__call__.apply(newInstance, arguments);
-				};
-				
-				for(name in this) {
-					callableObject[name] = this[name];
-				}
-				
-				return callableObject;
-			}
-		};
-		
-		// attach the static properties
-		for(name in metaClass) {
-			// Copy the static methods over onto the class
-			__class__[name] = typeof metaClass[name] == "function" && typeof __class__[name] == "function" && fnTest.test(metaClass[name]) ?
-				(function(name, fn){
-					return function() {
-						var
-							tmp= this._super
-							,ret
-						;
-						
-						this._super= _super_class[name];
-						ret= fn.apply(this, arguments);
-						this._super= tmp;
-						
-						return ret;
-					};
-				})(name, metaClass[name])
-				:
-				metaClass[name];
-		}
-		
-		// Instantiate a base class (but only create the instance,
-		// don't run the init constructor)
-		klassInitializing= true;
-		prototype= new this();
-		klassInitializing= false;
-		
-		for(name in kwargs) {
-			// Copy the properties over onto the new prototype
-			// Check if we're overwriting an existing function
-			prototype[name]= typeof kwargs[name] == "function" && typeof _super[name] == "function" && fnTest.test(kwargs[name]) ?
-				(function(name, fn){
-					return function() {
-						var tmp = this._super,
-							ret;
-						
-						// Add a new ._super() method that is the same method
-						// but on the super-class
-						this._super= _super[name];
-						
-						// The method only need to be bound temporarily, so we
-						// remove it when we're done executing
-						ret= fn.apply(this, arguments);
-						this._super= tmp;
-						
-						return ret;
-					};
-				})(name, kwargs[name])
-				:
-				kwargs[name];
-		}
-		
-		// Populate our constructed prototype object
-		__class__.prototype= prototype;
-		__class__.prototype.__class__= __class__;
-		
-		// Enforce the constructor to be what we expect
-		__class__.constructor= __class__;
-		
-		// And make this class extendable
-		for(name in this){
-			if(this.hasOwnProperty(name) && name != 'prototype'){
-				__class__[name] = this[name];
-			}
-		}
-		__class__.create= arguments.callee;
-		
-		if(kwargs && kwargs.__name__ && kwargs.__parent__) {
-			var
-				current= kwargs.__parent__,
-				parts= kwargs.__name__.split(/\./)
-			;
-			
-			for(i= 0; i < parts.length - 1; i++){
-				current = current[parts[i]] || ( current[parts[i]] = {} );
-			}
-			
-			current[parts[parts.length - 1]]= __class__;
-			__class__.__name__= parts[parts.length - 1];
-		}
-		
-		if(_super_class.extended) {
-			_super_class.extended(__class__);
-		}
-		
-		return __class__;
-	};
-	
-	// add a global meta class to the attributes
-	attrs.__metaclass__= Class.create();
-	
-	__global__.Class= Class;
+    var
+        attrs= {
+            __new__: function(classObj, kwargs){
+                // the instantiator method responsible to give back an instance
+                // of the class 'classObj'
+                
+                return new classObj(kwargs);
+            }
+            /*,__init__: function(kwargs){
+                var name;
+                
+                for(name in kwargs) {
+                    this[name]= kwargs[name];
+                }
+                
+                return this;
+            }*/
+            
+            ,__name__: 'Class'
+            
+            // be careful when using __call__
+            // the function provided will be called with the first two parameters
+            // being the current context object and the second argument being
+            // the actual callable object
+            ,__call__: null
+            ,__parent__: null
+            
+            ,__iter__: function(){ throw new NotImplemented() }
+            
+            ,__len__: function(){
+                // The __len__ method returns the length of the object
+                // or the length
+                var
+                    nKeys= 0
+                    ,name
+                ;
+                
+                // does it have a length attribute?
+                if(this.length !== undefined) {
+                    return this.length;
+                }
+                
+                // find out what's the length of this object based on own properties
+                for(name in this) {
+                    if(this.hasOwnProperty(name)) {
+                        nKeys+= 1;
+                    }
+                }
+                
+                return nKeys;
+            }
+            
+            ,__repr__: function(){
+                return "<" + this.__class__.__name__ + ": " + this.__class__.__name__ + " object>";
+            }
+            ,__str__: function(){
+                return this.__repr__.apply(this, arguments);
+            }
+            ,__unicode__: function(){ throw new NotImplemented() }
+            
+            // js stuff
+            ,toString: function(){
+                return this.__repr__.apply(this, arguments);
+            }
+            //,__iterator__: function(){
+            //    if(this.__iter__) {
+            //        return this.__iter__.apply(this, arguments);
+            //    }
+            //    
+            //    return new Iterator(this);
+            //}
+            //,toLocaleString: function(){}
+            //,toSource: function(){}
+            //,valueOf: function(){}
+        }
+        ,staticAttrs= {
+            __repr__: function(){
+                return "<class " + this.__name__ + ">";
+            }
+            ,toString: function(){
+                return this.__repr__.apply(this, arguments);
+            }
+        }
+        ,NotImplemented= new Error('Not implemented.')
+        ,klassInitializing= false
+        ,fnTest= /xyz/.test(function(){var xyz;}) ? /\b_super\b/ : /.*/
+        ,Class= function(kwargs){
+            return;
+        }
+    ;
+    
+    // Create a new Class that inherits from the current class.
+    Class.create= function(kwargs) {
+        var
+            _super_class= this
+            ,_super= this.prototype
+            ,prototype
+            ,metaClass= {}
+            ,name
+            ,__class__
+            ,instanceInitializing= false
+        ;
+        
+        // build the prototype
+        for(name in attrs) {
+            if(kwargs && !kwargs.hasOwnProperty(name)) {
+                kwargs[name]= attrs[name];
+            }
+        }
+        
+        // The dummy class constructor
+        // All construction is actually done in the __new__ method
+        __class__= function(){
+            var
+                newMethod
+                ,newInstance
+                ,args= Array.prototype.slice.call(arguments)
+                ,name
+                ,callableObject
+            ;
+            args.unshift(__class__);
+            
+            if(!(this instanceof __class__)) {
+                newMethod= __class__.prototype.__new__;
+            } else if(!instanceInitializing) {
+                newMethod= this.__new__;
+            }
+            
+            if( !(this instanceof __class__) || !instanceInitializing ) {
+                instanceInitializing= true;
+                newInstance= newMethod.apply(__class__, args);
+                instanceInitializing= false;
+                
+                if ( !klassInitializing && newInstance.__init__ && newInstance instanceof __class__ ) {
+                    newInstance.__init__.apply(newInstance, arguments);
+                }
+                
+                if(!newInstance.__call__) {
+                    return newInstance;
+                }
+                
+                // allow the new instance of the class to be callable
+                // it's good to make callable objects, but the returned
+                // object not being an instance of __class__
+                callableObject= function(){
+                    return newInstance.__call__.apply(newInstance, [this, callableObject].concat(Array.prototype.slice.call(arguments)));
+                };
+                
+                for(name in newInstance) {
+                    callableObject[name] = newInstance[name];
+                }
+                
+                return callableObject;
+            }
+        };
+        
+        // Instantiate a base class (but only create the instance,
+        // do not run the init constructor)
+        klassInitializing= true;
+        prototype= new this();
+        klassInitializing= false;
+        
+        for(name in kwargs) {
+            if(kwargs.hasOwnProperty(name)) {
+                
+                // Copy the properties over onto the new prototype
+                prototype[name]= (function(proto, name, kwargs){
+                    
+                    // Check if we're overwriting an existing function
+                    if(typeof kwargs[name] == "function" && typeof _super[name] == "function" && fnTest.test(kwargs[name])) {
+                        return function() {
+                            var
+                                tmp = this._super
+                                ,ret
+                            ;
+                            
+                            // Add a new ._super() method that is the same method
+                            // but on the super-class
+                            this._super= _super[name];
+                            
+                            // The method only need to be bound temporarily, so we
+                            // remove it when we're done executing
+                            ret= kwargs[name].apply(this, arguments);
+                            this._super= tmp;
+                            
+                            return ret;
+                        };
+                        
+                    } else {
+                        
+                        // copy the attribute to the prototype
+                        return kwargs[name];
+                        
+                    }
+                    
+                })(prototype, name, kwargs);
+                
+            }
+        }
+        
+        // Populate our constructed prototype object
+        __class__.prototype= prototype;
+        __class__.prototype.__class__= __class__;
+        
+        // Enforce the constructor to be what we expect
+        __class__.constructor= __class__;
+        
+        // And make this class extendable
+        for(name in this){
+            if(this.hasOwnProperty(name) && name != 'prototype'){
+                __class__[name] = this[name];
+            }
+        }
+        __class__.create= arguments.callee;
+        
+        if(kwargs && kwargs.__name__) {
+            var
+                current= kwargs.__parent__ || __global__
+                ,parts= kwargs.__name__.split(/\./)
+            ;
+            
+            for(i= 0; i < parts.length - 1; i++){
+                current = current[parts[i]] || ( current[parts[i]] = {} );
+            }
+            
+            current[parts[parts.length - 1]]= __class__;
+            __class__.__name__= parts[parts.length - 1];
+            __class__.__fullname__= kwargs.__name__;
+        }
+        /*
+        if(kwargs.__name__) {
+            __class__.__name__= kwargs.__name__.split('.').slice(-1)[0];
+        }
+        */
+        if(_super_class.extended) {
+            _super_class.extended(__class__);
+        }
+        
+        // metaclass init
+        if(kwargs && kwargs.__metaclass__) {
+            metaClass= kwargs.__metaclass__(__class__);
+        } else if(_super.__metaclass__) {
+            metaClass= _super.__metaclass__(__class__);
+        }
+        
+        // add static attributes and methods
+        for(name in staticAttrs) {
+            if(!metaClass.hasOwnProperty(name)) {
+                metaClass[name]= staticAttrs[name];
+            }
+        }
+        for(name in kwargs) {
+            if(kwargs[name] && kwargs.hasOwnProperty(name) && kwargs[name].staticMethod) {
+                metaClass[name]= kwargs[name];
+                delete kwargs[name];
+            }
+        }
+        
+        // attach the static properties
+        for(name in metaClass) {
+            if(__class__[name] === undefined) {
+                // Copy the static methods over onto the class
+                __class__[name] = typeof metaClass[name] == "function" && typeof __class__[name] == "function" && fnTest.test(metaClass[name]) ?
+                    (function(name, fn){
+                        return function() {
+                            var
+                                tmp= this._super
+                                ,ret
+                            ;
+                            
+                            this._super= _super_class[name];
+                            ret= fn.apply(this, arguments);
+                            this._super= tmp;
+                            
+                            return ret;
+                        };
+                    })(name, metaClass[name])
+                    :
+                    metaClass[name];
+            }
+        }
+        
+        return __class__;
+    };
+    
+    __global__.Class= Class;
 })(this);
